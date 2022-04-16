411338af86ed30cd9ad383536a549ea218fcd4dc
jennymont <niacmonteiro@gmail.com>
2022-04-16 00:56:46 -0300

    subindo projeto

---
+++ src/components/Listas/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+function Listas() {
+    const tarefas = [{
+        tarefa: "React",
+        tempo: "02:00:00", //obj 1
+    },
+    {
+        tarefa: "Javascritp",
+        tempo: "01:00:00", //obj 2
+    },
+
+    ]
+    return (
+
+        <aside >
+            <h2>
+                Estudos do dia
+            </h2>
+
+            <ul>
+                {tarefas.map((item, index) => (
+                    <li key={index}>
+                        <h3>
+                            {item.tarefa}
+                        </h3>
+                        <span>
+                            {item.tempo}
+                        </span>
+                    </li>
+
+                ))}
+            </ul>
+
+            
+
+        </aside>
+
+
+    )
+
+}
+
+export default Listas;
---
+++ src/pages/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import Formularios from '../components/Formularios';
+import Listas from '../components/Listas';
+import  './style.scss';
+
+
+function App() {
+  return (
+    <div className="AppStyle">
+      
+    <Formularios/>  
+    <Listas/>    
+    </div>
+  );
+}
+
+export default App;
